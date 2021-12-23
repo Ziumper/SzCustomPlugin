b4f4604a7ffe18676ebb175bfc0d7c72fc4239c2
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2021-12-23 11:50:30 +0100

    Define default json controller working

---
+++ src/Controller/ExampleJsonController.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+
+namespace SzCustomPlugin\Controller;
+
+use Shopware\Core\Framework\Context;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Example json controller class
+ * @RouteScope(scopes={"storefront"}) 
+ */
+class ExampleJsonController extends AbstractController {
+
+
+    /**
+     * Example json controller class get data method
+     * 
+     * XMLHttpRequest - is required for security reasons 
+     * 
+     * @Route("sz/json",name="frontend.sz.json", methods={"GET"},defaults={"XMLHttpRequest"=true})
+     *
+     * @param Request $request
+     * @param Context $context
+     * @return JsonResponse
+     */
+    public function getData(Request $request, Context $context) : JsonResponse {
+        return new JsonResponse([
+            'foo' => 'bar'
+        ]);
+    }
+}
