1af2ec70f0d897bf3b21673c8f98bb32f30b79bc
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2022-01-05 14:27:04 +0100

    Add example api route definitions in swagger

--- src/Route/ExampleRoute.php
+++ src/Route/ExampleRoute.php
@@ -2,12 +2,14 @@
 
 namespace SzCustomPlugin\Route;
 
+use OpenApi\Annotations\Get;
 use OpenApi\Annotations as OA;
 use Shopware\Core\System\SalesChannel\StoreApiResponse;
 use SzCustomPlugin\Route\Response\ExampleResponse;
 use SzCustomPlugin\Struct\ExampleStruct;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Core\Framework\Routing\Annotation\Since;
 
 /**
  * @RouteScope(scopes="store-api")
@@ -16,6 +18,20 @@ class ExampleRoute {
 
     /**
      * 
+     * 
+     * 
+     * @Since("6.3.0.0")
+     * @Get(
+     *  path="/example",
+     *      summary="Example route",
+     *      description="Example resource route",
+     *      operationId="exampleResource",
+     *      tags={"Store API", "Example"},
+     *      @OA\Response(
+     *          response="200",
+     *          description="Example"
+     *     )
+     * )
      * @OA\Info(title="My First API", version="0.1")
      * @Route("store-api/example", name="store-api.example", methods={"GET", "POST"})
      */
